c647077e9a2023ec01df23e3db66206f04de08c2
Oliver 2wist <joliver017@gmail.com>
2019-03-02 18:30:25 -0600

    added default search for movie if there's no input for movie name

--- liri.js
+++ liri.js
@@ -11,8 +11,8 @@ var args = process.argv;
 var searchArr = args.slice(3);
 var search = searchArr.join(" ");
 
-var queryUrl_1 = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
-var queryUrl_2 = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
+var bands_URL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp"
+var movies_URL = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
 
 
 // This is the axios request for BandsInTown
@@ -21,7 +21,7 @@ if (process.argv[2] == "concert-this") {
         console.log("====================");
 
 axios
-    .get(queryUrl_1)
+    .get(bands_URL)
     .then(function(response){
         for (i=0; i<response.data.length; i++) {
         // var momentDate = moment(response.data[i].datetime);
@@ -44,13 +44,18 @@ axios
 
 // This is the axios request for OMDB
 if (process.argv[2] == "movie-this") {
+    if (!search) {
+        search = "Mr Nobody";
+        movies_URL = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
+    }
     console.log(search);
     console.log("====================");
+    
     
 
 axios
 
-.get(queryUrl_2)
+.get(movies_URL)
 .then(function(response){
     console.log("Title: " + response.data.Title);
     console.log("Year Released: " + response.data.Year);
@@ -74,6 +79,9 @@ axios
 if (process.argv[2] == "spotify-this-song") {
     console.log(search);
     console.log("====================");
+    if (!search) {
+        search = "The Sign";
+    }
 
     spotify
     .search({ type: 'track', query: search, limit: 1 })
@@ -89,6 +97,12 @@ if (process.argv[2] == "spotify-this-song") {
     });
 };
 
+
+
+
+
+
+
 // This is the request for executing what the txt file says
 if (process.argv[2] == "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function(err, data) {
@@ -96,11 +110,84 @@ if (process.argv[2] == "do-what-it-says") {
           return console.log(err);
         }
 
+        var readMe_bands_URL = "https://rest.bandsintown.com/artists/" + data[1] + "/events?app_id=codingbootcamp"
+        var readMe_movies_URL = "http://www.omdbapi.com/?t=" + data[1] + "&y=&plot=short&apikey=trilogy";
+
         data = data.split(", ");
         console.log(data);
 
-    })
-}
+        if (data[0] == "spotify-this-song") {
+            console.log(data[1]);
+            console.log("====================");
+        
+            spotify
+            .search({ type: 'track', query: data[1], limit: 1 })
+            .then(function(response) {
+              console.log("Artist's Name: " + response.tracks.items[0].album.artists[0].name);
+              console.log("Song Name: " + response.tracks.items[0].name);
+              console.log("Album Name: " + response.tracks.items[0].album.name);
+              console.log("Song Link: " + response.tracks.items[0].album.artists[0].external_urls.spotify);
+            //   console.log(response.tracks.items);
+            })
+            .catch(function(err) {
+              console.log(err);
+            });
+        };
+
+        if (data[0] == "movie-this") {
+            console.log(data[1]);
+            console.log("====================");
+            
+        
+        axios
+        
+        .get(readMe_movies_URL)
+        .then(function(response){
+            console.log("Title: " + response.data.Title);
+            console.log("Year Released: " + response.data.Year);
+            console.log(response.data.Ratings[0].Source + ": " + response.data.Ratings[0].Value);
+            console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value);
+            console.log(response.data.Country);
+            console.log(response.data.Language);
+            console.log(response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+        
+        .catch(function(error){
+            if (error.response) {
+                console.log(error);
+            }
+        })
+        };
+
+        if (process.argv[2] == "concert-this") {
+            console.log(search);
+            console.log("====================");
+    
+    axios
+        .get(readMe_bands_URL)
+        .then(function(response){
+            for (i=0; i<response.data.length; i++) {
+            // var momentDate = moment(response.data[i].datetime);
+            // var date = moment(momentDate).format('MMMM Do YYYY, h:mm:ss a');
+            var venue_info = response.data[i].venue.name + "\n" + response.data[i].venue.city + ", " + response.data[i].venue.region + "\n" + response.data[i].datetime;
+           
+            console.log(venue_info);
+            console.log("====================")
+            // console.log(date);
+            }
+        })
+    
+        .catch(function(error){
+            if (error.response) {
+                console.log(error);
+            }
+        })
+    };
+        
+
+    });
+};
 
 // function defaultSearch () {
 //     if (process.argv[2] == "spotify-this-song" && search == "") {
